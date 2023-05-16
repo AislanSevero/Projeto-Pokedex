fff75fab53ab7ca2098b5b9b4ba87a8ec9367160
AislanSevero <129214278+AislanSevero@users.noreply.github.com>
2023-05-15 21:18:52 -0300

    Finalização do botão mudar tema no JS

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-alert("ola");
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+	const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+	body.classList.toggle("modo-escuro");
+
+	if (modoEscuroEstaAtivo) {
+		imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/sun.png");
+	}
+	else {
+		imagemBotaoTrocaDeTema.setAttribute("src", "./imagens/moon.png");
+	}
+});
